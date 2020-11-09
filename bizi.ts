028984d9c6d6d59e3e501136d24c36eafa568f8a
maticzav <matic.zavadlal@gmail.com>
2020-11-09 17:41:47 +0100

    :construction: bizi scrapper

--- bizi.ts
+++ bizi.ts
@@ -1,9 +1,8 @@
 import puppeteer from 'puppeteer'
 
-const URL = 'https://bizi.si'
 const CREDS = {
-    username: "maticzav",
-    password: "ac8b2a34"
+  username: 'maticzav',
+  password: 'ac8b2a34',
 }
 
 /**
@@ -12,54 +11,85 @@ const CREDS = {
 
 ;(async () => {
   const browser = await puppeteer.launch({
-    executablePath: 'google-chrome-stable',
+    // executablePath: 'google-chrome-stable',
     headless: false,
-    args: [
-      // Required for Docker version of Puppeteer
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      // This will write shared memory files into /tmp instead of /dev/shm,
-      // because Docker’s default for /dev/shm is 64MB
-      '--disable-dev-shm-usage',
-    ],
+    // args: [
+    //   // Required for Docker version of Puppeteer
+    //   '--no-sandbox',
+    //   '--disable-setuid-sandbox',
+    //   // This will write shared memory files into /tmp instead of /dev/shm,
+    //   // because Docker’s default for /dev/shm is 64MB
+    //   '--disable-dev-shm-usage',
+    // ],
   })
 
+  const page = await browser.newPage()
+  await page.goto('https://bizi.si')
 
-    const page = await browser.newPage()
-    await page.goto(URL)
+  console.log('Logging in!')
 
-    console.log('Logging in!')
+  /* Login */
+  await page.waitForSelector('#ctl00_LoginStatus1_PanelAnon > a')
 
-    /* Login */
-    await page.waitForSelector('#ctl00_LoginStatus1_PanelAnon')
+  await page.evaluate((_) => {
+    // @ts-ignore
+    showLoginModal()
+  })
 
-    console.log("Typing...")
+  console.log('Typing...')
 
-    await page.click("#ctl00_LoginStatus1_PanelAnon > a")
+  await page.waitForSelector('.login-form')
+  await page.waitForTimeout(1000)
 
-    await page.type('#ctl00_loginBoxPopup_loginBoxAlt_tbUserName', CREDS.username)
-    await page.type('#ctl00_loginBoxPopup_loginBoxAlt_tbPassword', CREDS.password)
+  await page.type(
+    '#ctl00_loginBoxPopup_loginBoxAlt_tbUserName',
+    CREDS.username,
+    {
+      delay: 1,
+    },
+  )
+  await page.type(
+    '#ctl00_loginBoxPopup_loginBoxAlt_tbPassword',
+    CREDS.password,
+    {
+      delay: 1,
+    },
+  )
 
-    await page.click("#ctl00_loginBoxPopup_loginBoxAlt_btnLogin")
+  await page.click('#ctl00_loginBoxPopup_loginBoxAlt_btnLogin')
 
-    console.log("Waiting...")
+  console.log('Waiting...')
 
-    /* Wait for authenticated page */
-    await page.waitForSelector("#ctl00_Search1_tbSearchWhat")
+  console.log('Logged in!')
 
-    console.log("Logged in!")
+  /* Find data */
 
-    /* Reserve */
-    // await page.waitFor('input#selectAllSwitch1')
-    // await page.click('input#selectAllSwitch1')
-    // await page.click('.card.rounded-0 button')
+  await page.goto('https://www.bizi.si/napredno-iskanje/')
 
-    // await page.waitFor('#feedback.toast')
+  console.log('Searching...')
 
-    // await page.click('a[href="/rezervacije-odjava"]')
+  await page.waitForSelector('#ctl00_SearchAdvanced1_btnPoisciPodjetja')
 
+  await page.select('#ctl00_SearchAdvanced1_SteviloZaposlenihOd', '07')
+  await page.waitForTimeout(1000)
+  await page.click('#ctl00_SearchAdvanced1_btnPoisciPodjetja')
 
+  console.log('Displaying results...')
 
+  // await page.waitForNavigation()
 
-//   await browser.close()
+  // javascript: setTimeout(
+  //   "__doPostBack('ctl00$SearchAdvanced1$SteviloZaposlenihOd','')",
+  //   0,
+  // )
+  /* Reserve */
+  // await page.waitFor('input#selectAllSwitch1')
+  // await page.click('input#selectAllSwitch1')
+  // await page.click('.card.rounded-0 button')
+
+  // await page.waitFor('#feedback.toast')
+
+  // await page.click('a[href="/rezervacije-odjava"]')
+
+  // await browser.close()
 })()
